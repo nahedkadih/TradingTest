0b41fa7d931fa5ba819aa2a7bce9084c0e53370b
nahedkadih <nkadih@yahoo.com>
2022-02-13 19:02:11 -0500

    posted

--- TradingAlgorthimsTests/UnitTest1.cs
+++ TradingAlgorthimsTests/UnitTest1.cs
@@ -54,43 +54,43 @@ namespace TestProject3
             //Assert.AreEqual("TRADE order1 1000 10 order3 900 10",
             //    TradingEngineDictionary3.DebugOutput[TradingEngineDictionary3.DebugOutput.Count - 1], "Case0080");
 
-            TradingEngineDictionary33.Reset();
+            TradingEngine0.Reset();
 
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1000 10 order1");
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1001 20 order2");
-            TradingEngineDictionary33.ExecuteCommand("PRINT");
+            TradingEngine0.ExecuteCommand("BUY GFD 1000 10 order1");
+            TradingEngine0.ExecuteCommand("BUY GFD 1001 20 order2");
+            TradingEngine0.ExecuteCommand("PRINT");
             Assert.AreEqual("1001 20",
                 Global.DebugOutput[Global.DebugOutput.Count - 2], "Case0090");
             Assert.AreEqual("1000 10",
                 Global.DebugOutput[Global.DebugOutput.Count - 1], "Case0100");
 
 
-            TradingEngineDictionary33.Reset();
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1000 10 order1");
-            TradingEngineDictionary33.ExecuteCommand("SELL GFD 900 20 order2");
-            TradingEngineDictionary33.ExecuteCommand("PRINT");
+            TradingEngine0.Reset();
+            TradingEngine0.ExecuteCommand("BUY GFD 1000 10 order1");
+            TradingEngine0.ExecuteCommand("SELL GFD 900 20 order2");
+            TradingEngine0.ExecuteCommand("PRINT");
             Assert.AreEqual("TRADE order1 1000 10 order2 900 10",
                 Global.DebugOutput[Global.DebugOutput.Count - 4], "Case0110");
             Assert.AreEqual("900 10",
                 Global.DebugOutput[Global.DebugOutput.Count - 2], "Case0120");
 
 
-            TradingEngineDictionary33.Reset();
+            TradingEngine0.Reset();
 
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1000 10 order1");
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1010 10 order2");
-            TradingEngineDictionary33.ExecuteCommand("SELL GFD 1000 15 order3");
+            TradingEngine0.ExecuteCommand("BUY GFD 1000 10 order1");
+            TradingEngine0.ExecuteCommand("BUY GFD 1010 10 order2");
+            TradingEngine0.ExecuteCommand("SELL GFD 1000 15 order3");
             Assert.AreEqual("TRADE order2 1010 10 order3 1000 10",
                 Global.DebugOutput[Global.DebugOutput.Count - 2], "Case0130");
             Assert.AreEqual("TRADE order1 1000 5 order3 1000 5",
                 Global.DebugOutput[Global.DebugOutput.Count - 1], "Case0140");
 
-            TradingEngineDictionary33.Reset();
+            TradingEngine0.Reset();
 
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1000 10 order1");
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1001 10 order2");
-            TradingEngineDictionary33.ExecuteCommand("SELL IOC 1000 15 order3");
-            TradingEngineDictionary33.ExecuteCommand("SELL GFD 1000 15 order4");
+            TradingEngine0.ExecuteCommand("BUY GFD 1000 10 order1");
+            TradingEngine0.ExecuteCommand("BUY GFD 1001 10 order2");
+            TradingEngine0.ExecuteCommand("SELL IOC 1000 15 order3");
+            TradingEngine0.ExecuteCommand("SELL GFD 1000 15 order4");
 
             Assert.AreEqual("TRADE order2 1001 10 order3 1000 10",
                 Global.DebugOutput[Global.DebugOutput.Count - 3], "Case0150");
@@ -101,13 +101,13 @@ namespace TestProject3
 
 
 
-            TradingEngineDictionary33.Reset();
+            TradingEngine0.Reset();
 
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1000 10 order1");
-            TradingEngineDictionary33.ExecuteCommand("SELL IOC 900 15 order2");
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1001 10 order3");
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1004 10 order4");
-            TradingEngineDictionary33.ExecuteCommand("SELL GFD 1000 15 order5");
+            TradingEngine0.ExecuteCommand("BUY GFD 1000 10 order1");
+            TradingEngine0.ExecuteCommand("SELL IOC 900 15 order2");
+            TradingEngine0.ExecuteCommand("BUY GFD 1001 10 order3");
+            TradingEngine0.ExecuteCommand("BUY GFD 1004 10 order4");
+            TradingEngine0.ExecuteCommand("SELL GFD 1000 15 order5");
 
             Assert.AreEqual("TRADE order1 1000 10 order2 900 10",
                  Global.DebugOutput[Global.DebugOutput.Count - 3], "Case0180");
@@ -117,15 +117,15 @@ namespace TestProject3
                  Global.DebugOutput[Global.DebugOutput.Count - 1], "Case0200");
 
             //TradeTest8.txt
-            TradingEngineDictionary33.Reset();
+            TradingEngine0.Reset();
 
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1000 25 order1");
-            TradingEngineDictionary33.ExecuteCommand("SELL IOC 900 15 order4");
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1001 10 order2");
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1004 10 order3");
-            TradingEngineDictionary33.ExecuteCommand("MODIFY order1 BUY 1005 10");
-            TradingEngineDictionary33.ExecuteCommand("SELL GFD 1000 15 order5");
-            TradingEngineDictionary33.ExecuteCommand("PRINT");
+            TradingEngine0.ExecuteCommand("BUY GFD 1000 25 order1");
+            TradingEngine0.ExecuteCommand("SELL IOC 900 15 order4");
+            TradingEngine0.ExecuteCommand("BUY GFD 1001 10 order2");
+            TradingEngine0.ExecuteCommand("BUY GFD 1004 10 order3");
+            TradingEngine0.ExecuteCommand("MODIFY order1 BUY 1005 10");
+            TradingEngine0.ExecuteCommand("SELL GFD 1000 15 order5");
+            TradingEngine0.ExecuteCommand("PRINT");
 
             Assert.AreEqual("TRADE order1 1000 15 order4 900 15",
                  Global.DebugOutput[0], "Case0210");
@@ -143,17 +143,17 @@ namespace TestProject3
                  Global.DebugOutput[6], "Case0270");
 
             //TradeTest9.txt
-            TradingEngineDictionary33.Reset();
+            TradingEngine0.Reset();
 
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1000 6 order1");
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1001 5 order2");
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1004 5 order3");
-            TradingEngineDictionary33.ExecuteCommand("SELL IOC 900 15 order4");
-            TradingEngineDictionary33.ExecuteCommand("MODIFY order1 BUY 1005 5");
-            TradingEngineDictionary33.ExecuteCommand("SELL GFD 1000 15 order5");
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 1006 5 order7");
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 800 5 order8");
-            TradingEngineDictionary33.ExecuteCommand("PRINT");
+            TradingEngine0.ExecuteCommand("BUY GFD 1000 6 order1");
+            TradingEngine0.ExecuteCommand("BUY GFD 1001 5 order2");
+            TradingEngine0.ExecuteCommand("BUY GFD 1004 5 order3");
+            TradingEngine0.ExecuteCommand("SELL IOC 900 15 order4");
+            TradingEngine0.ExecuteCommand("MODIFY order1 BUY 1005 5");
+            TradingEngine0.ExecuteCommand("SELL GFD 1000 15 order5");
+            TradingEngine0.ExecuteCommand("BUY GFD 1006 5 order7");
+            TradingEngine0.ExecuteCommand("BUY GFD 800 5 order8");
+            TradingEngine0.ExecuteCommand("PRINT");
 
             Assert.AreEqual("TRADE order3 1004 5 order4 900 5",
                 Global.DebugOutput[0], "Case0280");
@@ -172,11 +172,11 @@ namespace TestProject3
 
 
             //TradeTest10.txt
-            TradingEngineDictionary33.Reset();
+            TradingEngine0.Reset();
 
-            TradingEngineDictionary33.ExecuteCommand("SELL GFD 1000 5 order5");
-            TradingEngineDictionary33.ExecuteCommand("BUY GFD 800 5 order8");
-            TradingEngineDictionary33.ExecuteCommand("PRINT");
+            TradingEngine0.ExecuteCommand("SELL GFD 1000 5 order5");
+            TradingEngine0.ExecuteCommand("BUY GFD 800 5 order8");
+            TradingEngine0.ExecuteCommand("PRINT");
 
             Assert.AreEqual("1000 5",
                  Global.DebugOutput[1], "Case0350");
